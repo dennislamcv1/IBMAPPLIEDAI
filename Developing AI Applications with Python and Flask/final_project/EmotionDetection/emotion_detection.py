e3b3fd2b93df791e09ba739b64027aeca205cba3
Dennis Lam <39684447+dennislamcv1@users.noreply.github.com>
2023-08-13 12:31:45 +0800

    Add files via upload

--- Developing AI Applications with Python and Flask/final_project/EmotionDetection/emotion_detection.py
+++ Developing AI Applications with Python and Flask/final_project/EmotionDetection/emotion_detection.py
@@ -8,10 +8,23 @@ def emotion_detector(text_to_analyse):
    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
    myobj = {"raw_document": { "text": text_to_analyse } }
    response = requests.post(url, json = myobj, headers = headers)
-   res = json.loads(response.text)
-   formatted_output = res['emotionPredictions'][0]['emotion']
-   dominant_emotion = max(formatted_response, key = lambda x: formatted_response[x])
-   formatted_response['dominant_dictionary'] = dominant_emotion
+   status_code = response.status_code
+   
+   if status_code == 400:
+        formatted_output = { 'anger': None,
+                             'disgust': None,
+                             'fear': None,
+                             'joy': None,
+                             'sadness': None,
+                             'dominant_emotion': None }
+        
+   
+   else:
+        res = json.loads(response.text)
+        formatted_output = res['emotionPredictions'][0]['emotion']
+        dominant_emotion = max(formatted_response, key = lambda x: formatted_response[x])
+        formatted_response['dominant_dictionary'] = dominant_emotion
+   
    return formatted_response
 
 
